77585b7c363ab2ca231d3dcb46749cb6f72c99c3
sahilsingh-7 <164621582+sahilsingh-7@users.noreply.github.com>
2024-05-30 21:06:25 +0530

    Create removeDuplicateFromArrayL26.cpp

---
+++ removeDuplicateFromArrayL26.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i,j = 1;
+        for(i=1;i<nums.size();i++){
+            if(nums[i] != nums[i-1]){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+};
