a35738e6ff562d1a330da54d79be0a75ce89fbb4
sahilsingh-7 <164621582+sahilsingh-7@users.noreply.github.com>
2024-06-25 20:42:43 +0530

    Create BSTtoGreaterSumTree.cpp

---
+++ BSTtoGreaterSumTree.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sum = 0;
+    void calc(TreeNode *root){
+        
+        if(!root) {
+            return;
+        }
+        calc(root->right);
+        sum += root->val;
+        root->val = sum;
+        calc(root->left);
+    }
+    TreeNode* bstToGst(TreeNode* root) {
+        calc(root);
+        return root;
+    }
+};
